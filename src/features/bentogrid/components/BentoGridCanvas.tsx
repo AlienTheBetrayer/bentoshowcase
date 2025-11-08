a9cdd1ab1ebdf0635101a81d4e9ddf9c37377d5c
alienthebetrayer <gpichkurov@gmail.com>
2025-11-08 16:29:54 +0200

    Better canvas light + dragging cursor

--- src/features/bentogrid/components/BentoGridCanvas.tsx
+++ src/features/bentogrid/components/BentoGridCanvas.tsx
@@ -1,25 +1,39 @@
 import { Canvas } from '@react-three/fiber';
 import './BentoGridCanvas.css';
 
-import { RoundedBox } from '@react-three/drei';
+import { OrbitControls, RoundedBox } from '@react-three/drei';
 import { motion } from 'motion/react';
+import { useState } from 'react';
 
 export const BentoGridCanvas = () => {
+    const [isDragging, setIsDragging] = useState<boolean>(false);
+
     return (
         <motion.div className='bento-grid-canvas-container'>
             <Canvas
                 style={{
+                    cursor: isDragging ? 'grabbing' : 'grab',
                     width: '100%',
                     height: '100%',
                     background: 'transparent',
                 }}
+                camera={{
+                    position: [0, 0, 10],
+                    fov: 45,
+                }}
                 gl={{ alpha: true }}
+                onPointerDown={() => setIsDragging(true)}
+                onPointerLeave={() => setIsDragging(false)}
+                onPointerUp={() => setIsDragging(false)}
             >
-                <pointLight position={[0, 0, 0]} intensity={128} />
+                <pointLight position={[-4, 7, 5]} intensity={128} />
+                <hemisphereLight />
 
                 <RoundedBox position={[0, 0, 0]} args={[3, 6, 3]}>
                     <meshPhysicalMaterial />
                 </RoundedBox>
+
+                <OrbitControls enableZoom={false} />
             </Canvas>
         </motion.div>
     );
