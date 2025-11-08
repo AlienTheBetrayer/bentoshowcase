5c5a4b9e797fc780da4a639b8eab62d169d9deb8
alienthebetrayer <gpichkurov@gmail.com>
2025-11-08 22:40:30 +0200

    Card aberration effect on hover

--- src/features/bentogrid/components/selected/BentoSelectedCardCanvas.tsx
+++ src/features/bentogrid/components/selected/BentoSelectedCardCanvas.tsx
@@ -1,5 +1,11 @@
 import { Center, OrbitControls } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
+import {
+    ChromaticAberration,
+    EffectComposer,
+} from '@react-three/postprocessing';
+import { ChromaticAberrationEffect } from 'postprocessing';
+import React, { useEffect, useRef } from 'react';
 import type { BentoGridBox } from '../../context/types/BentoTypes';
 import { BentoSelectedCardReplica } from './BentoSelectedCardReplica';
 
@@ -8,9 +14,25 @@ interface Props {
 }
 
 export const BentoSelectedCardCanvas = ({ box }: Props) => {
+    const isHoveredRef = useRef<boolean>(false);
+
     return (
-        <Canvas style={{ width: '100%', height: '100%'}}>
+        <Canvas
+            style={{ width: '100%', height: '100%' }}
+            onPointerEnter={() => {
+                isHoveredRef.current = true;
+            }}
+            onPointerLeave={() => {
+                isHoveredRef.current = false;
+            }}
+        >
             <pointLight position={[3, 3, 3]} intensity={128} />
+
+            <EffectComposer>
+                <BentoSelectedCardCanvasAberration
+                    isHoveredRef={isHoveredRef}
+                />
+            </EffectComposer>
 
             <Center>
                 <BentoSelectedCardReplica box={box} />
@@ -20,3 +42,32 @@ export const BentoSelectedCardCanvas = ({ box }: Props) => {
         </Canvas>
     );
 };
+
+interface AberrationProps {
+    isHoveredRef: React.RefObject<boolean>;
+}
+
+const BentoSelectedCardCanvasAberration = React.memo(
+    ({ isHoveredRef }: AberrationProps) => {
+        const aberrationRef = useRef<ChromaticAberrationEffect>(null);
+
+        useEffect(() => {
+            if (aberrationRef.current) {
+                aberrationRef.current.offset.x = 0;
+                aberrationRef.current.offset.y = 0;
+            }
+        }, []);
+
+        useFrame(() => {
+            const targetOffset = isHoveredRef.current ? 0.3 : 0;
+            if (aberrationRef.current) {
+                aberrationRef.current.offset.x +=
+                    (targetOffset - aberrationRef.current.offset.x) * 0.1;
+                aberrationRef.current.offset.y +=
+                    (targetOffset - aberrationRef.current.offset.y) * 0.1;
+            }
+        });
+
+        return <ChromaticAberration ref={aberrationRef} offset={[0, 0]} />;
+    }
+);
