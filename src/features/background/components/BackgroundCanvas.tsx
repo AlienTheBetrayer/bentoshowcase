f5cd5147c3de5dac87314fd94cb2a737de5cf295
alienthebetrayer <gpichkurov@gmail.com>
2025-11-10 16:38:58 +0200

    FPS warning

--- src/features/background/components/BackgroundCanvas.tsx
+++ src/features/background/components/BackgroundCanvas.tsx
@@ -1,11 +1,14 @@
+import './BackgroundCanvas.css';
 import { Canvas } from '@react-three/fiber';
 import { Bloom, EffectComposer } from '@react-three/postprocessing';
+
 import { useRef, useState } from 'react';
 import { useCursorRef } from '../../../hooks/useCursorRef';
 import { useMediaQuery } from '../../../hooks/useMediaQuery';
 import { useLocalStore } from '../../../zustand/localStore';
 import { BackgroundLight } from './BackgroundLight';
 import { BackgroundParticles } from './BackgroundParticles';
+import { motion } from 'motion/react';
 
 export const BackgroundCanvas = () => {
     const pointer = useCursorRef({ x: 200, y: 600 });
@@ -17,30 +20,46 @@ export const BackgroundCanvas = () => {
     const [isLagging, setIsLagging] = useState<boolean>(false);
 
     return (
-        <Canvas
-            style={{ width: '100%', height: '100%', pointerEvents: 'none' }}
-        >
-            {theme === 'dark' && !isMobile && !isLagging && (
-                <EffectComposer>
-                    <Bloom emissiveThreshold={0} emissiveIntensity={64} />
-                </EffectComposer>
+        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+            {theme === 'dark' && isLagging && (
+                <motion.div
+                    className='background-fps-warning'
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                >
+                    <p>
+                        <small>
+                            Effects <u>reduced!</u>
+                        </small>
+                    </p>
+                </motion.div>
             )}
 
-            <BackgroundParticles
-                pointer={pointer}
-                onFpsChange={(fps) => {
-                    if (fps < 30) {
-                        performanceTimeout.current = setTimeout(
-                            () => setIsLagging(true),
-                            1000
-                        );
-                    } else if (performanceTimeout.current !== false) {
-                        clearTimeout(performanceTimeout.current);
-                        performanceTimeout.current = false;
-                    }
-                }}
-            />
-            <BackgroundLight pointer={pointer} />
-        </Canvas>
+            <Canvas
+                style={{ width: '100%', height: '100%', pointerEvents: 'none' }}
+            >
+                {theme === 'dark' && !isMobile && !isLagging && (
+                    <EffectComposer>
+                        <Bloom emissiveThreshold={0} emissiveIntensity={64} />
+                    </EffectComposer>
+                )}
+
+                <BackgroundParticles
+                    pointer={pointer}
+                    onFpsChange={(fps) => {
+                        if (fps < 20) {
+                            performanceTimeout.current = setTimeout(
+                                () => setIsLagging(true),
+                                3000
+                            );
+                        } else if (performanceTimeout.current !== false) {
+                            clearTimeout(performanceTimeout.current);
+                            performanceTimeout.current = false;
+                        }
+                    }}
+                />
+                <BackgroundLight pointer={pointer} />
+            </Canvas>
+        </div>
     );
 };
--- src/features/bentogrid/components/BentoGridCanvas.tsx
+++ src/features/bentogrid/components/BentoGridCanvas.tsx
@@ -61,6 +61,7 @@ export const BentoGridCanvas = () => {
             onPointerDown={handlePointerDown}
             onPointerUp={handlePointerUp}
         >
+            
             <Canvas
                 style={{
                     width: '100%',
