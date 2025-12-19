5af7550285a6a8c9fc3c09c76c93cf40dd46b777
alienthebetrayer <gpichkurov@gmail.com>
2025-12-19 13:49:11 +0200

    Analytics SDK integrated

--- src/features/header/components/Header.tsx
+++ src/features/header/components/Header.tsx
@@ -1,56 +1,66 @@
-import { useLoadingContext } from '../../loading/context/LoadingContext';
-import './Header.css';
+import { useLoadingContext } from "../../loading/context/LoadingContext";
+import "./Header.css";
 
-import { motion } from 'motion/react';
-import { useLocalStore } from '../../../zustand/localStore';
-import { LinkButton } from '../../ui/LinkButton/components/LinkButton';
-import { ToggleButton } from '../../ui/ToggleButton/components/ToggleButton';
-import { useHeaderAnimation } from '../hooks/useHeaderAnimation';
+import { useAnalytics } from "@alienthebetrayer/analytics-sdk-react";
+import { motion } from "motion/react";
+import { useLocalStore } from "../../../zustand/localStore";
+import { LinkButton } from "../../ui/LinkButton/components/LinkButton";
+import { ToggleButton } from "../../ui/ToggleButton/components/ToggleButton";
 
-import homeImg from '../assets/home.svg';
-import pricingImg from '../assets/pricing.svg';
+import homeImg from "../assets/home.svg";
+import pricingImg from "../assets/pricing.svg";
+import { useHeaderAnimation } from "../hooks/useHeaderAnimation";
 
 export const headerSize = 300;
 
 export const Header = () => {
-    const [loadingState] = useLoadingContext();
-    const { theme, toggleTheme } = useLocalStore();
+	const [loadingState] = useLoadingContext();
+	const { theme, toggleTheme } = useLocalStore();
 
-    useHeaderAnimation();
+	useHeaderAnimation();
 
-    return (
-        <motion.header
-            whileHover={
-                loadingState.hasHeaderFinished
-                    ? { maxWidth: `${headerSize + 24}px` }
-                    : {}
-            }
-        >
-            <nav>
-                <LinkButton to='/home'>
-                    <img
-                        src={homeImg}
-                        alt='home'
-                        className='img'
-                        style={{ opacity: 0.5 }}
-                    />
-                    Home
-                </LinkButton>
-                <LinkButton to='/pricing'>
-                    <img
-                        src={pricingImg}
-                        alt='pricing'
-                        className='img'
-                        style={{ opacity: 0.5 }}
-                    />
-                    Pricing
-                </LinkButton>
-                <ToggleButton
-                    value={theme !== 'dark'}
-                    onChange={() => toggleTheme()}
-                    style={{ marginLeft: 'auto' }}
-                />
-            </nav>
-        </motion.header>
-    );
+	// analytics
+	const analytics = useAnalytics();
+
+	return (
+		<motion.header
+			whileHover={
+				loadingState.hasHeaderFinished
+					? { maxWidth: `${headerSize + 24}px` }
+					: {}
+			}
+		>
+			<nav>
+				<LinkButton to="/home">
+					<img
+						src={homeImg}
+						alt="home"
+						className="img"
+						style={{ opacity: 0.5 }}
+					/>
+					Home
+				</LinkButton>
+				<LinkButton to="/pricing">
+					<img
+						src={pricingImg}
+						alt="pricing"
+						className="img"
+						style={{ opacity: 0.5 }}
+					/>
+					Pricing
+				</LinkButton>
+				<ToggleButton
+					value={theme !== "dark"}
+					onChange={() => {
+						if (theme === "dark") {
+							analytics.current.send("theme_toggle");
+						}
+
+						toggleTheme();
+					}}
+					style={{ marginLeft: "auto" }}
+				/>
+			</nav>
+		</motion.header>
+	);
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import './index.css';
-import { GlobalRouter } from './router';
-import { LocalStoreWatcher } from './zustand/LocalStoreWatcher';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <>
-            <LocalStoreWatcher />
-            <RouterProvider router={GlobalRouter} />
-        </>
-    </StrictMode>
+import { AnalyticsProvider } from "@alienthebetrayer/analytics-sdk-react";
+import { GlobalRouter } from "./router";
+import { LocalStoreWatcher } from "./zustand/LocalStoreWatcher";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<AnalyticsProvider config={{ endpoint: "bento" }}>
+			<LocalStoreWatcher />
+			<RouterProvider router={GlobalRouter} />
+		</AnalyticsProvider>
+	</StrictMode>,
 );
